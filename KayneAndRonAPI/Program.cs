8061a2de92bc44f890b87b792e15db59cbd9e168
Cody <Cojo341@gmail.com>
2022-09-09 14:31:17 -0400

    Add project files.

---
+++ KayneAndRonAPI/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Newtonsoft.Json.Linq;
+using static System.Net.WebRequestMethods;
+
+
+var client = new HttpClient();
+
+var kanyeURL = "https://api.kanye.rest/";
+
+var ronclient = new HttpClient();
+
+var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+for (int i = 0; i < 5; i++)
+{
+    var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+    var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+    var ronResponse = ronclient.GetStringAsync(ronURL).Result;
+
+    var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Replace("\"", "").Trim();
+
+
+    Console.WriteLine($"Kayne: {kanyeQuote}");
+    
+    Console.WriteLine($"Ron: {ronQuote}\n");
+}
+
+
